b4663ef9eea9a7c8d0bb474d9f60e3378cf75197
Roberto Burruezo <roberto.burruezom@gmail.com>
2026-05-17 13:09:12 +0200

    Add: redundant-connection - submission-2

---
+++ Data Structures & Algorithms/redundant-connection/submission-2.py
@@ -0,0 +1,39 @@
+class Solution:
+    def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
+        n = len(edges)
+        adj = [[] for _ in range(n + 1)]
+        for u, v in edges:
+            adj[u].append(v)
+            adj[v].append(u)
+
+        visit = [False] * (n + 1)
+        cycle = set()
+        self.cycleStart = -1
+
+        def dfs(node, par):
+            if visit[node]:
+                self.cycleStart = node
+                return True
+            
+            visit[node] = True
+            for nei in adj[node]:
+                if nei == par:
+                    continue
+                if dfs(nei, node):
+                    if self.cycleStart != -1:
+                        cycle.add(node)
+                    if node == self.cycleStart:
+                        self.cycleStart = -1
+                    return True
+            return False
+        
+        dfs(1, -1)
+
+        for u, v in reversed(edges):
+            if u in cycle and v in cycle:
+                return [u, v]
+        
+        return []
+
+        
+
